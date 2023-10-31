2ba8286293376c891559193414cbff3196c3bc46
RuiboSurface <zrbbit@gmail.com>
2023-10-30 23:28:01 -0400

    followup

--- DataExtraction/scripts/chembl_process_data_v2.py
+++ DataExtraction/scripts/chembl_process_data_v2.py
@@ -15,7 +15,6 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
 
-from utils.args import ChemblPipelineArgs
 
 from rdkit import Chem
 from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers, StereoEnumerationOptions
